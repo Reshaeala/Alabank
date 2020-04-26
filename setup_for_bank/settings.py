fd28000e778422296e698aa188436410ae673787
Reshaeala <58646198+Reshaeala@users.noreply.github.com>
2020-04-25 22:41:22 -0500

    Ready for deployment

--- setup_for_bank/settings.py
+++ setup_for_bank/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -101,7 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+django_heroku.settings(locals())
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
