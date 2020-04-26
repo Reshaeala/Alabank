40ae333ae9554f5250a687659aecbc81011f2487
Reshaeala <58646198+Reshaeala@users.noreply.github.com>
2020-04-26 15:39:18 -0500

    adding db

--- setup_for_bank/settings.py
+++ setup_for_bank/settings.py
@@ -82,9 +82,13 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+import psycopg2
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
